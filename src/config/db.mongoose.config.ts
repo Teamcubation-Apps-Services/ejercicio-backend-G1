8a41aa61d3b8060bd2ca09c2f920036387b4446e
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2022-11-17 13:02:02 -0300

    feat: Client Schema added

--- src/config/db.mongoose.config.ts
+++ src/config/db.mongoose.config.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const db = async () => {
-	try {
-		await mongoose.connect(process.env.DB_MONGO_HOST!);
-		console.log("DB Mongo connected!");
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    await mongoose.connect(process.env.DB_MONGO_HOST!);
+    console.log("DB Mongo connected!");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default db;
