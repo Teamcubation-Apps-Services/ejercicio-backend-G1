f304ebfde21707f4fb2bfe681da7c23df32d0d8a
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2022-11-17 16:41:49 -0300

    refactor: minimal modification to mongodb connection

--- src/config/db.mongoose.config.ts
+++ src/config/db.mongoose.config.ts
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
-const db = async () => {
+(async () => {
   try {
     await mongoose.connect(process.env.DB_MONGO_HOST!);
     console.log("DB Mongo connected!");
   } catch (error) {
     console.log(error);
   }
-};
-
-export default db;
+})();
