437e3777fcce8889a1f27c582efe9f0e95a6c720
Dario <dario.gomez@iovlabs.org>
2022-11-23 12:58:14 -0300

    fix: added missing parameter to balance crud

--- src/repository/sequelize/balance.repository.sequelize.ts
+++ src/repository/sequelize/balance.repository.sequelize.ts
@@ -1,19 +1,19 @@
 import Balance from "../../models/sequelize/balance.model.sequelize";
 
-async function find(client: string) {
-  return await Balance.findByPk(client);
+async function find(client: string, crypto: any) {
+  return await Balance.findOne({ where: { client, crypto } });
 }
 
 async function create(balance: any) {
   return await Balance.create(balance);
 }
 
-async function update(client: string, balance: any) {
-  return await Balance.update(balance, { where: { client } });
+async function update(client: string, crypto: any, balance: any) {
+  return await Balance.update(balance, { where: { client, crypto } });
 }
 
-async function remove(client: string) {
-  return await Balance.destroy({ where: { client } });
+async function remove(client: string, crypto: any) {
+  return await Balance.destroy({ where: { client, crypto } });
 }
 
 const balanceRepository = {
