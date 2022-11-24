81e1ae01cf9a3adbe73143e2cfa6d63e02527141
Dario <dario.gomez@iovlabs.org>
2022-11-24 13:41:03 -0300

    fix: typed all model arguments in all sequelize repositories

--- src/repository/sequelize/balance.repository.sequelize.ts
+++ src/repository/sequelize/balance.repository.sequelize.ts
@@ -1,18 +1,23 @@
 import Balance from "../../models/sequelize/balance.model.sequelize";
+import { SequelizeBalance } from "../../sequelizeTypes";
 
 async function find(client: string, crypto: any) {
   return await Balance.findOne({ where: { client, crypto } });
 }
 
-async function create(balance: any) {
+async function create(balance: SequelizeBalance) {
   return await Balance.create(balance);
 }
 
-async function update(client: string, crypto: any, balance: any) {
+async function update(
+  client: string,
+  crypto: string,
+  balance: SequelizeBalance
+) {
   return await Balance.update(balance, { where: { client, crypto } });
 }
 
-async function remove(client: string, crypto: any) {
+async function remove(client: string, crypto: string) {
   return await Balance.destroy({ where: { client, crypto } });
 }
 
--- src/repository/sequelize/client.repository.sequelize.ts
+++ src/repository/sequelize/client.repository.sequelize.ts
@@ -1,14 +1,15 @@
 import Client from "../../models/sequelize/client.model.sequelize";
+import { SequelizeClient } from "../../sequelizeTypes";
 
 async function find(name: string) {
   return await Client.findByPk(name);
 }
 
-async function create(client: any) {
+async function create(client: SequelizeClient) {
   return await Client.create(client);
 }
 
-async function update(name: string, client: any) {
+async function update(name: string, client: SequelizeClient) {
   return await Client.update(client, { where: { name } });
 }
 
