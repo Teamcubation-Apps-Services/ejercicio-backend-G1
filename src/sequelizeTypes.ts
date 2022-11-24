683f695bd3957430683003ce59eb9ef469fbd26a
Dario <dario.gomez@iovlabs.org>
2022-11-24 13:32:32 -0300

    feat: added file with sequelize models types for typescript

---
+++ src/sequelizeTypes.ts
@@ -0,0 +1,44 @@
+type SequelizeBalance = {
+  client: string;
+  amount: number;
+  cryptocurrency: string;
+};
+
+type SequelizeClient = {
+  name: string;
+  lastName: string;
+  email: string;
+  dni: number;
+  phone: number;
+};
+
+type SequelizeCryptocurrency = {
+  name: string;
+  price: number;
+  anualRevenue: number;
+  description: string;
+};
+
+type SequelizeMovement = {
+  from: string;
+  type: string;
+  amount: number;
+  fee: number;
+  to: string;
+  cryptocurrency: string;
+};
+
+type SequelizeWallet = {
+  name: string;
+  discount: number;
+  maxRefund: number;
+  vigency: string;
+};
+
+export {
+  SequelizeBalance,
+  SequelizeClient,
+  SequelizeCryptocurrency,
+  SequelizeMovement,
+  SequelizeWallet,
+};
